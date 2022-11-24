4a3071359f17dedf0f404afcedb27636166e0da8
masanbat12 <masanbat12@gmail.com>
2022-11-24 23:35:38 +0200

    done

--- NumClass.h
+++ NumClass.h
@@ -2,11 +2,11 @@
 #include <math.h>
 #define false 0
 #define true 1
-
+// is a functio to check if number is  armstrong by loop and recursion
 int isArmstrong(int n);
-
+// is a functio to check if number is  prime
 int isPrime(int n);
-
+// is a functio to check if number is  palindrom by loop and recursion
 int isPalindrome(int n);
-
+// is a functio to check if number is  strong
 int isStrong(int n);
